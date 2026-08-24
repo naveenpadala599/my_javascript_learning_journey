32e08f5ec0f2cf279dcf73c44b4d13662f8dc9bb
naveenpadala599 <58857810+naveenpadala599@users.noreply.github.com>
2026-08-24 22:40:27 +0530

    Add example of method calls and 'this' context
    Demonstrate method call and standalone call behavior with 'this'.

---
+++ this-method-call-standalone-call-call.js
@@ -0,0 +1,19 @@
+const person = {
+
+    name: "Alice",
+
+    sayHello: function(){
+
+       console.log(this.name);
+
+    }
+
+};
+
+person.sayHello();
+
+const fn = person.sayHello;
+
+fn();
+
+fn.call(person);
